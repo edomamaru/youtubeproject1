310107f97874f37594bdad6bd94b825bf36456d2
Edom Maru <edomamaru@gmail.com>
2023-07-04 00:20:50 +0000

    commit

--- project1.py
+++ project1.py
@@ -6,9 +6,23 @@
 
 from pyyoutube import Api
 import json
+import sqlalchemy as db
+import pandas as pd
 
 api = Api(api_key="AIzaSyAnarBmG-NnwVJl8IpJcE3A1XmebCspit0")
 channel_by_id = api.get_channel_info(channel_id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
 print(channel_by_id.items)
-channel_object = json.dumps(channel_by_id.items[0].to_dict(),indent=4)
-print(channel_object)
+#channel_object = json.dumps(channel_by_id.items[0].to_dict(),indent=4)
+channel_dict = channel_by_id.items[0].to_dict()
+json_str = json.dumps(channel_dict, indent=4)
+# print(type(channel_dict))
+dataframe_name=pd.DataFrame.from_dict({'chan':json_str},orient='index')
+engine = db.create_engine('sqlite:///data_base_name.db')
+dataframe_name.to_sql('table_name', con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name"))
+   columns = query_result.keys()
+   #print(columns)
+   print(pd.DataFrame(query_result))
+
+
